02ff1f63a6ea872af03c98a3e1f8dee985aa59ef
Neev100 <79758801+Neev100@users.noreply.github.com>
2021-08-17 17:28:33 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+noseX = "";
+noseY = "";
+
+function preload() {
+    clown_nose = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
+}
+function setup() {
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console.log("Posenet initialized")
+}
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x - 25;
+        noseY = results[0].pose.nose.y - 20;
+        console.log(noseX);
+        console.log(noseY);
+    }
+}
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX, noseY, 50,50);
+}
+function take_snapshot() {
+    save("myfilterimage.png")
+}
